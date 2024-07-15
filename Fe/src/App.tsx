885ae6cc9cca77e57eaa09ba576023d98002f617
Jun-K-code <JunCode@aliyun.com>
2024-07-15 17:19:00 +0800

    feat: Fe-项目初始化

---
+++ Fe/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Login from './pages/login';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
